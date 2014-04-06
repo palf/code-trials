4b54ff14cbbf6f19e4b404d89568eb0ba683c83f
Jack Palfrey <jack.palf@gmail.com>
2014-04-06 00:31:28 -0700

    created grunt tasks to support development (lint, test, watch)

---
+++ tasks/lint.js
@@ -0,0 +1,24 @@
+var config = {
+    options: {
+        jshintrc: '.jshintrc'
+    },
+
+    all: {
+        src: [
+            'src/*.js',
+            'src/**/*.js',
+            'test/*.js',
+            'test/**/*.js',
+            'server/*.js',
+
+            'gruntfile.js',
+            'tasks/*.js'
+        ]
+    }
+};
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.renameTask('jshint', 'lint');
+    grunt.config('lint', config);
+};
---
+++ tasks/watch.js
@@ -0,0 +1,18 @@
+var config = {
+    options: { atBegin: true },
+
+    lint: {
+        files: '<%= lint.all.src %>',
+        tasks: [ 'lint' ]
+    },
+
+    test: {
+        files: [ '<%= test.all %>', 'src/**/*.js' ],
+        tasks: [ 'test' ]
+    }
+};
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.config('watch', config);
+};
