633e1fc4debead524f84fb464f831a158b10379f
Jack Palfrey <jack.palf@gmail.com>
2014-12-21 16:39:51 +0000

    added invalid account exception handling

--- src/rewardService.js
+++ src/rewardService.js
@@ -1,14 +1,4 @@
-var InvalidAccountNumber = {
-    message: "invalid account number",
-    rewards: []
-};
-
-var InvalidPortfolio = {
-    message: "invalid portfolio",
-    rewards: []
-};
-
-var ValidResponse = {
+var EligibleResponse = {
     message: "successful",
     rewards: []
 };
@@ -18,28 +8,72 @@ var NotEligibleResponse = {
     rewards: []
 };
 
+var InvalidAccountNumberResponse = {
+    message: "invalid account number",
+    rewards: []
+};
+
+var InvalidPortfolioResponse = {
+    message: "invalid portfolio",
+    rewards: []
+};
+
 var UnavailableResponse = {
     message: "eligibility service is unreachable",
     rewards: []
 };
 
-function performEligibilityCheck (check) {
+var UnknownErrorResponse = {
+    message: "eligibility service experienced an unknown error",
+    rewards: []
+};
+
+
+var EligibilityStatus = {
+    eligible: 'CUSTOMER_ELIGIBLE',
+    ineligible: 'CUSTOMER_INELIGIBLE',
+    invalidAccountNumber: 'INVALID_ACCOUNT_NUMBER',
+    unavailable: 'SERVICE_UNAVAILABLE',
+    unknownError: 'UNKNOWN_ERROR'
+};
+
+function identifyError (error) {
+    if (error.name === 'invalid account number') {
+        return EligibilityStatus.invalidAccountNumber;
+    } else if (error.name === 'technical failure') {
+        return EligibilityStatus.unavailable;
+    } else {
+        return EligibilityStatus.unknownError;
+    }
+}
+
+function performEligibilityCheck (check, account) {
     var status;
     try {
-        status = check();
+        status = check(account);
     } catch (error) {
-        status = 'SERVICE_UNAVAILABLE';
+        status = identifyError(error);
     }
     return status;
 }
 
-function handleEligibiltyStatus (status) {
-    if (status === 'CUSTOMER_INELIGIBLE') {
-        return NotEligibleResponse;
-    } else if (status === 'SERVICE_UNAVAILABLE') {
-        return UnavailableResponse;
-    } else {
-        return ValidResponse;
+
+function buildResponseForStatus (status) {
+    switch (status) {
+        case EligibilityStatus.eligible:
+            return EligibleResponse;
+
+        case EligibilityStatus.ineligible:
+            return NotEligibleResponse;
+
+        case EligibilityStatus.unavailable:
+            return UnavailableResponse;
+
+        case EligibilityStatus.invalidAccountNumber:
+            return InvalidAccountNumberResponse;
+
+        default:
+            return UnknownErrorResponse;
     }
 }
 
@@ -47,12 +81,12 @@ function handleEligibiltyStatus (status) {
 function RewardService (eligibilityCheck) {
     this.fetchRewards = function (accountNumber, portfolio) {
         if (accountNumber === undefined) {
-            return InvalidAccountNumber;
+            return InvalidAccountNumberResponse;
         } else if (portfolio === undefined) {
-            return InvalidPortfolio;
+            return InvalidPortfolioResponse;
         } else {
-            var eligibilityStatus = performEligibilityCheck(eligibilityCheck);
-            return handleEligibiltyStatus(eligibilityStatus);
+            var status = performEligibilityCheck(eligibilityCheck, accountNumber);
+            return buildResponseForStatus(status);
         }
     };
 }
--- test/rewardService.js
+++ test/rewardService.js
@@ -4,13 +4,19 @@ var sinon = require('sinon');
 
 describe(".fetchRewards(accountNumber, portfolio)", function () {
     describe("invocation", function () {
-        var rewardService;
+        var eligibilityCheck, rewardService;
 
         beforeEach(function () {
-            rewardService = new RewardService();
+            eligibilityCheck = sinon.stub();
+            rewardService = new RewardService(eligibilityCheck);
         });
 
         describe("called without an account number", function () {
+            it("does not make an eligibility check", function () {
+                rewardService.fetchRewards();
+                expect(eligibilityCheck.called).to.be(false);
+            });
+
             it("returns an 'invalid account number' message", function() {
                 var response = rewardService.fetchRewards();
                 expect(response.message).to.be("invalid account number");
@@ -25,6 +31,11 @@ describe(".fetchRewards(accountNumber, portfolio)", function () {
 
         describe("called without a portfolio", function () {
             var validAccountNumber = 4;
+
+            it("does not make an eligibility check", function () {
+                rewardService.fetchRewards(validAccountNumber);
+                expect(eligibilityCheck.called).to.be(false);
+            });
 
             it("returns an 'invalid portfolio' message", function() {
                 var response = rewardService.fetchRewards(validAccountNumber);
@@ -62,19 +73,87 @@ describe(".fetchRewards(accountNumber, portfolio)", function () {
         });
     });
 
-    describe("when the eligibility service suffers a technical failure", function () {
-        var validAccountNumber = 4,
+
+    describe("when the eligibility service throws an 'invalid account' exception", function () {
+        var invalidAccountNumber = 4,
             emptyPortfolio = [],
+            eligibilityCheck,
             rewardService;
 
         beforeEach(function () {
-            var eligibilityCheck = sinon.stub().throws('technical failure');
+            eligibilityCheck = sinon.stub().throws('invalid account number');
             rewardService = new RewardService(eligibilityCheck);
+        });
+
+        it("makes an eligibility check with the account number", function () {
+            rewardService.fetchRewards(invalidAccountNumber, emptyPortfolio);
+            expect(eligibilityCheck.callCount).to.be(1);
+            expect(eligibilityCheck.calledWith(invalidAccountNumber)).to.be(true);
+        });
+
+        it("returns a 'supplied account number is invalid' message", function() {
+            var response = rewardService.fetchRewards(invalidAccountNumber, emptyPortfolio);
+            expect(response.message).to.be("invalid account number");
+        });
+
+        it("returns an empty list of results", function () {
+            var response = rewardService.fetchRewards(invalidAccountNumber, emptyPortfolio);
+            expect(response.rewards).to.be.an('array');
+            expect(response.rewards.length).to.be(0);
+        });
+    });
+
+
+    describe("when the eligibility service throws a technical failure", function () {
+        var validAccountNumber = 4,
+            emptyPortfolio = [],
+            eligibilityCheck,
+            rewardService;
+
+        beforeEach(function () {
+            eligibilityCheck = sinon.stub().throws('technical failure');
+            rewardService = new RewardService(eligibilityCheck);
+        });
+
+        it("makes an eligibility check with the account number", function () {
+            rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(eligibilityCheck.callCount).to.be(1);
+            expect(eligibilityCheck.calledWith(validAccountNumber)).to.be(true);
         });
 
         it("returns a 'eligibility service is unreachable' message", function() {
             var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
             expect(response.message).to.be("eligibility service is unreachable");
+        });
+
+        it("returns an empty list of results", function () {
+            var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(response.rewards).to.be.an('array');
+            expect(response.rewards.length).to.be(0);
+        });
+    });
+
+
+    describe("when the eligibility service throws an unknown error", function () {
+        var validAccountNumber = 4,
+            emptyPortfolio = [],
+            eligibilityCheck,
+            rewardService;
+
+        beforeEach(function () {
+            eligibilityCheck = sinon.stub().throws();
+            rewardService = new RewardService(eligibilityCheck);
+        });
+
+        it("makes an eligibility check with the account number", function () {
+            rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(eligibilityCheck.callCount).to.be(1);
+            expect(eligibilityCheck.calledWith(validAccountNumber)).to.be(true);
+        });
+
+        it("returns a 'eligibility service experienced an unknown error' message", function() {
+            var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(response.message).to.be("eligibility service experienced an unknown error");
         });
 
         it("returns an empty list of results", function () {
