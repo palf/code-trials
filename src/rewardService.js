10583beec5a215c4a2a88771abd838cd71ead536
Jack Palfrey <jack.palf@gmail.com>
2014-12-21 15:58:48 +0000

    added sinon stubbing lib and eligibilty service stub

--- src/rewardService.js
+++ src/rewardService.js
@@ -1,29 +1,61 @@
 var InvalidAccountNumber = {
     message: "invalid account number",
-    results: []
+    rewards: []
 };
 
 var InvalidPortfolio = {
     message: "invalid portfolio",
-    results: []
+    rewards: []
 };
 
 var ValidResponse = {
     message: "successful",
-    results: []
+    rewards: []
 };
 
-function fetchRewards (accountNumber, portfolio) {
-    if (accountNumber === undefined) {
-        return InvalidAccountNumber;
-    } else if (portfolio === undefined) {
-        return InvalidPortfolio;
+var NotEligibleResponse = {
+    message: "customer is not eligible",
+    rewards: []
+};
+
+var UnavailableResponse = {
+    message: "eligibility service is unreachable",
+    rewards: []
+};
+
+function performEligibilityCheck (check) {
+    var status;
+    try {
+        status = check();
+    } catch (error) {
+        status = 'SERVICE_UNAVAILABLE';
+    }
+    return status;
+}
+
+function handleEligibiltyStatus (status) {
+    if (status === 'CUSTOMER_INELIGIBLE') {
+        return NotEligibleResponse;
+    } else if (status === 'SERVICE_UNAVAILABLE') {
+        return UnavailableResponse;
     } else {
         return ValidResponse;
     }
 }
 
 
-module.exports = {
-    fetchRewards: fetchRewards
-};
+function RewardService (eligibilityCheck) {
+    this.fetchRewards = function (accountNumber, portfolio) {
+        if (accountNumber === undefined) {
+            return InvalidAccountNumber;
+        } else if (portfolio === undefined) {
+            return InvalidPortfolio;
+        } else {
+            var eligibilityStatus = performEligibilityCheck(eligibilityCheck);
+            return handleEligibiltyStatus(eligibilityStatus);
+        }
+    };
+}
+
+
+module.exports = RewardService;
--- test/rewardService.js
+++ test/rewardService.js
@@ -1,47 +1,86 @@
 var expect = require('expect.js');
-var rewardService = require('../src/rewardService.js');
+var RewardService = require('../src/rewardService.js');
+var sinon = require('sinon');
 
 describe(".fetchRewards(accountNumber, portfolio)", function () {
-    describe("with no account number", function () {
-        it("returns an 'invalid account number' message", function() {
-            var response = rewardService.fetchRewards();
-            expect(response.message).to.be("invalid account number");
+    describe("invocation", function () {
+        var rewardService;
+
+        beforeEach(function () {
+            rewardService = new RewardService();
         });
 
-        it("returns an empty list of results", function () {
-            var response = rewardService.fetchRewards();
-            expect(response.results).to.be.an('array');
-            expect(response.results.length).to.be(0);
+        describe("called without an account number", function () {
+            it("returns an 'invalid account number' message", function() {
+                var response = rewardService.fetchRewards();
+                expect(response.message).to.be("invalid account number");
+            });
+
+            it("returns an empty list of results", function () {
+                var response = rewardService.fetchRewards();
+                expect(response.rewards).to.be.an('array');
+                expect(response.rewards.length).to.be(0);
+            });
         });
-    });
 
-    describe("with a valid account number", function () {
-        var validAccountNumber = 4;
+        describe("called without a portfolio", function () {
+            var validAccountNumber = 4;
 
-        describe("but no portfolio", function () {
-            it("returns an 'portfolio not supplied' message", function() {
+            it("returns an 'invalid portfolio' message", function() {
                 var response = rewardService.fetchRewards(validAccountNumber);
                 expect(response.message).to.be("invalid portfolio");
             });
 
             it("returns an empty list of results", function () {
                 var response = rewardService.fetchRewards();
-                expect(response.results).to.be.an('array');
-                expect(response.results.length).to.be(0);
+                expect(response.rewards).to.be.an('array');
+                expect(response.rewards.length).to.be(0);
             });
         });
+    });
 
-        describe("with an empty portfolio", function () {
-            it("returns an 'successful' message", function() {
-                var response = rewardService.fetchRewards(validAccountNumber, []);
-                expect(response.message).to.be("successful");
-            });
 
-            it("returns an empty list of results", function () {
-                var response = rewardService.fetchRewards();
-                expect(response.results).to.be.an('array');
-                expect(response.results.length).to.be(0);
-            });
+    describe("when a customer is not eligible", function () {
+        var validAccountNumber = 4,
+            emptyPortfolio = [],
+            rewardService;
+
+        beforeEach(function () {
+            var eligibilityCheck = sinon.stub().returns('CUSTOMER_INELIGIBLE');
+            rewardService = new RewardService(eligibilityCheck);
+        });
+
+        it("returns a 'customer is not eligible' message", function() {
+            var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(response.message).to.be("customer is not eligible");
+        });
+
+        it("returns an empty list of results", function () {
+            var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(response.rewards).to.be.an('array');
+            expect(response.rewards.length).to.be(0);
+        });
+    });
+
+    describe("when the eligibility service suffers a technical failure", function () {
+        var validAccountNumber = 4,
+            emptyPortfolio = [],
+            rewardService;
+
+        beforeEach(function () {
+            var eligibilityCheck = sinon.stub().throws('technical failure');
+            rewardService = new RewardService(eligibilityCheck);
+        });
+
+        it("returns a 'eligibility service is unreachable' message", function() {
+            var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(response.message).to.be("eligibility service is unreachable");
+        });
+
+        it("returns an empty list of results", function () {
+            var response = rewardService.fetchRewards(validAccountNumber, emptyPortfolio);
+            expect(response.rewards).to.be.an('array');
+            expect(response.rewards.length).to.be(0);
         });
     });
 });
