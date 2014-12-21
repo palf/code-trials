e12b47bf970f6cf8d0b43c820c09859f43d7c1f3
Jack Palfrey <jack.palf@gmail.com>
2014-12-21 14:59:59 +0000

    added results for invalid portfolio calls

--- src/rewardService.js
+++ src/rewardService.js
@@ -1,8 +1,23 @@
-function fetchRewards () {
-    return {
-        message: "invalid account number",
-        results: []
-    };
+var InvalidAccountNumber = {
+    message: "invalid account number",
+    results: []
+};
+
+var InvalidPortfolio = {
+    message: "invalid portfolio",
+    results: []
+};
+
+
+
+function fetchRewards (accountNumber, portfolio) {
+    if (accountNumber === undefined) {
+        return InvalidAccountNumber;
+    }
+
+    if (portfolio === undefined) {
+        return InvalidPortfolio;
+    }
 }
 
 
--- test/rewardService.js
+++ test/rewardService.js
@@ -14,4 +14,20 @@ describe(".fetchRewards(customerNumber, portfolio)", function () {
             expect(response.results.length).to.be(0);
         });
     });
+
+    describe("with a valid account number but no portfolio", function () {
+        var validAccountNumber = 4;
+
+        it("returns an 'portfolio not supplied' message", function() {
+            var response = rewardService.fetchRewards(validAccountNumber);
+            expect(response.message).to.be("invalid portfolio");
+        });
+
+        it("returns an empty list of results", function () {
+            var response = rewardService.fetchRewards();
+            expect(response.results).to.be.an('array');
+            expect(response.results.length).to.be(0);
+        });
+    });
+
 });
